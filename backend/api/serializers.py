5778e5364415985dcce9e9a093f3a23f8b72716a
maxim190404 <maksipirami@gmail.com>
2025-06-06 20:21:55 +0300

    Внес исправления

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -10,6 +10,7 @@ from django.core.files.base import ContentFile
 import base64
 import uuid
 from rest_framework.exceptions import AuthenticationFailed
+import binascii
 
 User = get_user_model()
 
@@ -30,8 +31,19 @@ class Base64ImageField(serializers.ImageField):
         if isinstance(data, str) and data.startswith('data:image'):
             format, imgstr = data.split(';base64,')
             ext = format.split('/')[-1]
+
+            padding = len(imgstr) % 4
+            if padding:
+                imgstr += '=' * (4 - padding)
+
+            try:
+                decoded_file = base64.b64decode(imgstr)
+            except (TypeError, binascii.Error) as e:
+                raise serializers.ValidationError(
+                    "Некорректное изображение в формате base64") from e
+
             data = ContentFile(
-                base64.b64decode(imgstr),
+                decoded_file,
                 name=f'{uuid.uuid4()}.{ext}'
             )
         return super().to_internal_value(data)
